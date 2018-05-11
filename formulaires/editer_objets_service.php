413abfedf217694e19ebffbeea77863b22734e62
Rainer Müller <rainer@websimple.be>
2018-05-11 09:57:16 +0200

    publier directement

--- formulaires/editer_objets_service.php
+++ formulaires/editer_objets_service.php
@@ -69,6 +69,12 @@ function formulaires_editer_objets_service_identifier_dist($id_objets_service = 
  */
 function formulaires_editer_objets_service_charger_dist($id_objets_service = 'new', $retour = '', $associer_objet = '', $lier_trad = 0, $config_fonc = '', $row = array(), $hidden = '') {
 	$valeurs = formulaires_editer_objet_charger('objets_service', $id_objets_service, '', $lier_trad, $retour, $config_fonc, $row, $hidden);
+
+	// Publier directement
+	if ($id_objets_service == 'oui') {
+		$valeurs['_hidden'] .= '<input type="hidden" name="statut" value="publie" />';
+	}
+
 	return $valeurs;
 }
 
@@ -139,9 +145,9 @@ function formulaires_editer_objets_service_traiter_dist($id_objets_service = 'ne
 
 		if ($objet and $id_objet and autoriser('modifier', $objet, $id_objet)) {
 			include_spip('action/editer_liens');
-			
+
 			objet_associer(array('objets_service' => $id_objets_service), array($objet => $id_objet));
-			
+
 			if (isset($retours['redirect'])) {
 				$retours['redirect'] = parametre_url($retours['redirect'], 'id_lien_ajoute', $id_objets_service, '&');
 			}
