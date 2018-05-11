f541e634b892373524677b6bc3d2d24f2ca69f01
Rainer Müller <rainer@websimple.be>
2018-05-11 09:37:49 +0200

    afficher su la page des objets choisis

--- objets_services_extras_pipelines.php
+++ objets_services_extras_pipelines.php
@@ -31,13 +31,13 @@ if (!defined('_ECRIRE_INC_VERSION')) {
  * @return array       Données du pipeline
  */
 function objets_services_extras_affiche_milieu($flux) {
+	include_spip('inc/config');
 	$texte = '';
 	$e = trouver_objet_exec($flux['args']['exec']);
+	$objets_cibles = lire_config('objets_infos_extras/objets', array());
 
-
-
-	// objets_services sur les immeubles
-	if (!$e['edition'] and in_array($e['type'], array('immeuble'))) {
+	// objets_services sur les objets choisis
+	if (!$e['edition'] and in_array($e['table_objet_sql'], $objets_cibles)) {
 		$texte .= recuperer_fond('prive/objets/editer/liens', array(
 			'table_source' => 'objets_services',
 			'objet' => $e['type'],
